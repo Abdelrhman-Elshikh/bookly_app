99b4954268de2d331be2480e829203de2b0c292d
Abdelrhman-Elshikh <abdelrhmanelshikh@gmail.com>
2024-11-11 20:06:46 +0200

    add font family with dark theme

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,16 @@
-import 'package:bookly_app/core/utils/constants.dart';
-import 'package:bookly_app/feature/splash/presentation/views/splash_view.dart';
 import 'package:flutter/material.dart';
 import 'package:device_preview/device_preview.dart';
 import 'package:get/get.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:bookly_app/core/utils/constants.dart';
+import 'package:bookly_app/feature/splash/presentation/views/splash_view.dart';
 
 void main() {
-  runApp( DevicePreview(
-    builder: (context) =>const BooklyAPP(), // Wrap your app
-  ),);
+  runApp(
+    DevicePreview(
+      builder: (context) => const BooklyAPP(), // Wrap your app
+    ),
+  );
 }
 
 class BooklyAPP extends StatelessWidget {
@@ -19,7 +22,8 @@ class BooklyAPP extends StatelessWidget {
     return GetMaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData.dark().copyWith(
-        scaffoldBackgroundColor: KPrimaryColor
+        scaffoldBackgroundColor: KPrimaryColor,
+        textTheme: GoogleFonts.montserratTextTheme(ThemeData.dark().textTheme),
       ),
       home: const SplashView(),
     );
